c19116081d1357fa68a015a8dd08acf89da9bee3
Tobias Fischer <info@tobiasfischer.info>
2021-05-19 07:44:50 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 # Remove unwritable config.sub and config.guess first
-rm -rf ./config.*
+rm ./config.sub
+rm ./config.guess
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
